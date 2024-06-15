f078eab8311c367f7a6917e03cc3f56d7585b4d8
thecraftianman <64441307+thecraftianman@users.noreply.github.com>
2024-06-15 14:56:45 -0400

    Slight Benelli unnerf

--- lua/cfc_entity_stubber/arccw_uc/arccw_ud_m1014.lua
+++ lua/cfc_entity_stubber/arccw_uc/arccw_ud_m1014.lua
@@ -2,6 +2,6 @@ AddCSLuaFile()
 
 cfcEntityStubber.registerStub( function()
     local weapon = cfcEntityStubber.getWeapon( "arccw_ud_m1014" )
-    weapon.Damage = 15 -- Orig: 18
-    weapon.DamageMin = 7 -- Orig: 10
+    weapon.Damage = 16 -- Orig: 18
+    weapon.DamageMin = 8 -- Orig: 10
 end )
